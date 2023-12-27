e63af97c81cc6bf48f36dab77d5f88b854035000
Roman-K26 <romankuspys777@gmail.com>
2023-12-27 19:12:50 +0300

    email verification

--- src/schemas.py
+++ src/schemas.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from typing import List, Optional
-from pydantic import BaseModel, Field
-
+from pydantic import BaseModel, Field, EmailStr
 
 
 class ContactBase(BaseModel):
@@ -48,3 +47,6 @@ class TokenModel(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+class RequestEmail(BaseModel):
+    email: EmailStr
